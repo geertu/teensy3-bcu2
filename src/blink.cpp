18854e576012239ec5c7f26ee170381053c60d71
René Jeschke <rene_jeschke@yahoo.de>
2015-11-06 10:33:23 +0100

    Enhanced example source code

--- src/blink.cpp
+++ src/blink.cpp
@@ -1,25 +1,28 @@
 // This example is taken and modified from the Teensyduino 'Blink' sketch
 // See: https://www.pjrc.com/teensy/teensyduino.html
-
 #include <WProgram.h>
 
-const int ledPin = 13;
+static const int ledPin = 13;
+
+#define SKETCH_ALIKE
+
+#ifdef SKETCH_ALIKE
 
 // the setup() method runs once, when the sketch starts
 void setup()
 {
-    // initialize the digital pin as an output.
+    // initialize the digital pin for OUTPUT
     pinMode(ledPin, OUTPUT);
 }
 
-// the loop() methor runs over and over again,
+// the loop() method runs over and over again,
 // as long as the board has power
 void loop()
 {
-    digitalWrite(ledPin, HIGH);   // set the LED on
-    delay(1000);                  // wait for a second
-    digitalWrite(ledPin, LOW);    // set the LED off
-    delay(1000);                  // wait for a second
+    digitalWrite(ledPin, HIGH); // set the LED on
+    delay(500);                 // wait for half a second
+    digitalWrite(ledPin, LOW);  // set the LED off
+    delay(500);                 // wait for half a second
 }
 
 // This is needed as the starting point, as the standard main.cpp gets
@@ -34,3 +37,21 @@ extern "C" int main()
     }
 }
 
+#else
+
+extern "C" int main()
+{
+    // setup()
+    pinMode(ledPin, OUTPUT);
+    for (;;)
+    {
+        // loop()
+        digitalWrite(ledPin, HIGH); // set the LED on
+        delay(500);                 // wait for half a second
+        digitalWrite(ledPin, LOW);  // set the LED off
+        delay(500);                 // wait for half a second
+
+        yield();                    // yield() is mandatory!
+    }
+}
+#endif
