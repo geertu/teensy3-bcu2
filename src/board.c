geertu/teensy3-bcu2

commit 2cd6bcc534b192e4a67ecae795c702ed22cbc607

board: Abstract GPIO operations
Add a board API to control GPIO expansion, and use that instead of
direct pin accesses.

Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

src/board.c
@@ -22,8 +22,11 @@ static const uint8_t pin_power[NUM_POWER_CH] = { 7, 17 };
 
 static const uint8_t pin_key[NUM_KEY_CH] = { 8, 11, 12, 14, 15, 16 };
 
+static const uint8_t pin_gpio[NUM_GPIO_CH] = { 2, 23 };
+
 static char power_cache[NUM_POWER_CH];
 static char key_cache[NUM_KEY_CH];
+static char gpio_cache[NUM_GPIO_CH];
 
 void power_init(void)
 {
@@ -68,4 +71,23 @@ int key_get(unsigned int ch)
 	return key_cache[ch];
 }
 
-const uint8_t pin_gpio[NUM_GPIO_CH] = { 2, 23 };
+void gpio_init(void)
+{
+	unsigned int i;
+
+	for (i = 0; i < NUM_GPIO_CH; i++) {
+		pinMode(pin_gpio[i], OUTPUT);
+		digitalWrite(pin_gpio[i], 0);
+	}
+}
+
+void gpio_set(unsigned int ch, int on)
+{
+	digitalWrite(pin_gpio[ch], !on);
+	gpio_cache[ch] = on;
+}
+
+int gpio_get(unsigned int ch)
+{
+	return gpio_cache[ch];
+}

src/bcu2.c
@@ -183,17 +183,17 @@ static void test(void)
 		if (!ch)
 			power_set(NUM_POWER_CH - 1, 0);
 		else
-			digitalWrite(pin_gpio[ch - 1], 0);
+			gpio_set(ch - 1, 0);
 
 		printf("Pulsing GPIO %c\n", '0' + ch);
-		digitalWrite(pin_gpio[ch], 1);
+		gpio_set(ch, 1);
 		return;
 	}
 
 	ch -= NUM_GPIO_CH;
 	if (ch < NUM_UART_CH) {
 		if (!ch)
-			digitalWrite(pin_gpio[NUM_GPIO_CH - 1], 0);
+			gpio_set(NUM_GPIO_CH - 1, 0);
 
 		printf("Saying hello to UART channel %c\n", 'A' + ch);
 		if (!ch)
@@ -243,11 +243,6 @@ static void leds_init(void)
 
 	pinMode(pin_heartbeat, OUTPUT);
 
-	for (i = 0; i < NUM_GPIO_CH; i++) {
-		pinMode(pin_gpio[i], OUTPUT);
-		digitalWrite(pin_gpio[i], 0);
-	}
-
 	for (i = 0; i < NUM_RGB_CH * 3; i++) {
 		pinMode(pin_rgb[i], OUTPUT);
 		digitalWrite(pin_rgb[i], 1);
@@ -264,6 +259,7 @@ int main(int argc, char *argv[])
 	leds_init();
 	power_init();
 	key_init();
+	gpio_init();
 	twi_init();
 	measure_init();
 	console_init();

src/board.h
@@ -18,7 +18,6 @@ extern const uint8_t pin_heartbeat;
 #define NUM_UART_CH		2
 
 extern const uint8_t pin_rgb[];
-extern const uint8_t pin_gpio[];
 
 extern void power_init(void);
 extern void power_set(unsigned int ch, int on);
@@ -27,3 +26,7 @@ extern int power_get(unsigned int ch);
 extern void key_init(void);
 extern void key_set(unsigned int ch, int on);
 extern int key_get(unsigned int ch);
+
+extern void gpio_init(void);
+extern void gpio_set(unsigned int ch, int on);
+extern int gpio_get(unsigned int ch);

src/cmd.c
@@ -365,7 +365,6 @@ static void cmd_key(int argc, char *argv[])
 
 static void cmd_gpio(int argc, char *argv[])
 {
-	static char cache[NUM_GPIO_CH];
 	unsigned int i;
 	int ch, state;
 
@@ -383,7 +382,7 @@ static void cmd_gpio(int argc, char *argv[])
 
 	if (argc < 2) {
 		for_each_selected_channel(i, ch, NUM_GPIO_CH)
-			printf("%d\n", cache[i]);
+			printf("%d\n", gpio_get(i));
 		return;
 	}
 
@@ -395,22 +394,19 @@ static void cmd_gpio(int argc, char *argv[])
 		switch (state) {
 		case STATE_ON:
 			printf("Switching GPIO %c %s\n", '0' + i, "on");
-			digitalWrite(pin_gpio[i], 1);
-			cache[i] = 1;
+			gpio_set(i, 1);
 			break;
 
 		case STATE_OFF:
 			printf("Switching GPIO %c %s\n", '0' + i, "off");
-			digitalWrite(pin_gpio[i], 0);
-			cache[i] = 0;
+			gpio_set(i, 0);
 			break;
 
 		case STATE_PULSE:
 			printf("Pulsing GPIO %c\n", '0' + i);
-			digitalWrite(pin_gpio[i], 1);
+			gpio_set(i, 1);
 			delay(KEY_PULSE_MS);
-			digitalWrite(pin_gpio[i], 0);
-			cache[i] = 0;
+			gpio_set(i, 0);
 			break;
 		}
 	}

commit 47ed393de9774c3ba755b5d3e3ab4583634075ce

board: Fix inverted behavior of GPIO expansion
The behavior of the GPIO command was accidentally inverted.

Fixes: 2cd6bcc534b192e4 ("board: Abstract GPIO operations")
Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

src/board.c
@@ -105,7 +105,7 @@ void gpio_init(void)
 
 void gpio_set(unsigned int ch, int on)
 {
-	digitalWrite(pin_gpio[ch], !on);
+	digitalWrite(pin_gpio[ch], on);
 	gpio_cache[ch] = on;
 }
 